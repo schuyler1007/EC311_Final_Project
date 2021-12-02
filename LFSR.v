c5c2ac531a4ea8f93bdfde47c34327e5047da3e3
bennit6 <74574676+bennit6@users.noreply.github.com>
2021-12-02 17:36:24 -0500

    Update LFSR.v
    changes for backend

--- LFSR.v
+++ LFSR.v
@@ -2,19 +2,19 @@ module LFSR (clk_i, en_i, out_o);
     input clk_i, en_i;
     output [7:0] out_o;
 
-    reg [8:1] next_LFSR = 0;
+    reg [7:0] next_LFSR = 0;
     reg nextbit;
 
     always@(posedge clk_i) begin
         if (en_i == 1'b1) begin
-           next_LFSR <= {next_LFSR[7:1], nextbit}; 
+           next_LFSR <= {next_LFSR[6:0], nextbit}; 
         end
     end // always
 
     always@(*) begin
-        nextbit = next_LFSR[8] ^~ next_LFSR[6] ^~ next_LFSR[5] ^~ next_LFSR[4];
+        nextbit = next_LFSR[7] ^~ next_LFSR[5] ^~ next_LFSR[4] ^~ next_LFSR[3];
     end // always
 
-    assign out_o = next_LFSR[8:1];
+    assign out_o = next_LFSR[7:0];
 
 endmodule
