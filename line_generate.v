69c5938de1b638c0e6fa981209265eea948eda18
Kaede Kawata <85388805+schuyler1007@users.noreply.github.com>
2021-12-02 23:05:07 -0500

    Update line_generate.v

--- line_generate.v
+++ line_generate.v
@@ -2,9 +2,9 @@ module line_generate(reset_i, clk_i, en_i, line_o);
     input reset_i, clk_i, en_i;
     output [639:0] line_o;
     wire [7:0] rand_num;
-    reg [639:0] cs, ns;
-    reg [6:0] count;
-    reg new_bit;
+    reg [639:0] cs = {640{1'b1}}, ns = {640{1'b1}};
+    reg [6:0] count = 0;
+    reg new_bit = 1;
     
     LFSR rn(.clk_i(clk_i), .en_i(en_i), .out_o(rand_num));
 
