bb611bd4a91a9dd1cd0a8c563497727576d8a02d
Kaede Kawata <85388805+schuyler1007@users.noreply.github.com>
2021-12-03 15:33:54 -0500

    Update LFSR_tb.v

--- Testbenches/LFSR_tb.v
+++ Testbenches/LFSR_tb.v
@@ -3,7 +3,11 @@ module LFSR_tb();
     wire [7:0] out_o;
 
     LFSR lfsr_inst(.clk_i(clk_i), .en_i(1'b1), .out_o(out_o));
-
+    
+    initial begin
+        clk_i = 1'b0;
+    end
+    
     always@(*) begin
         #10 clk_i <= ~clk_i;
     end
