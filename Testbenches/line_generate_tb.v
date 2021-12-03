bd56457c455a43208960e22ad5fab40c6fb3dbfa
Kaede Kawata <85388805+schuyler1007@users.noreply.github.com>
2021-12-02 23:06:00 -0500

    Update line_generate_tb.v

--- Testbenches/line_generate_tb.v
+++ Testbenches/line_generate_tb.v
@@ -1,21 +1,17 @@
-module line_generate_tb();
-    reg clk_i, clk_lfsr;
+odule line_generate_tb();
+    reg clk_i, reset_i;
     wire [639:0] out_o;
-    wire [7:0] rand_num;
     
-    line_generate lg(.clk_i(clk_i), .clk_lfsr(clk_lfsr), .en_i(1'b1), .line_o(out_o), .rand_num(rand_num));
+    line_generate lg(.clk_i(clk_i),.reset_i(reset_i), .en_i(1'b1), .line_o(out_o));
     
     initial begin
         clk_i = 0;
-        clk_lfsr = 0;
+        #10 reset_i = 0;
+        reset_i = 1;
     end
     
     always begin
         #1 clk_i = ~clk_i;
     end
-    
-    always begin
-        #80 clk_lfsr = ~clk_lfsr;
-    end
-    
+   
 endmodule
