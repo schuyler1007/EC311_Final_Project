0b6f37fdf1c2b81d4afb334861c9c57cfaa75255
bennit6 <74574676+bennit6@users.noreply.github.com>
2021-12-05 13:30:28 -0500

    Update LFSR_tb.v
    added seed parameter

--- Testbenches/LFSR_tb.v
+++ Testbenches/LFSR_tb.v
@@ -1,11 +1,18 @@
 module LFSR_tb();
     reg clk_i;
-    wire [7:0] out_o;
+    wire out1, out2, out3;
 
-    LFSR lfsr_inst(.clk_i(clk_i), .en_i(1'b1), .out_o(out_o));
-
+    LFSR #(.seed(0)) lfsr_inst1(.clk_i(clk_i), .en_i(1'b1), .nextbit(out1));
+    LFSR #(.seed(4)) lfsr_inst2(.clk_i(clk_i), .en_i(1'b1), .nextbit(out2));
+    LFSR #(.seed(2)) lfsr_inst3(.clk_i(clk_i), .en_i(1'b1), .nextbit(out3));
+   
+    
+    initial begin
+        clk_i = 1'b0;
+    end
+    
     always@(*) begin
-        #10 clk_i <= ~clk_i;
+        #1 clk_i <= ~clk_i;
     end
 
 endmodule
