c4a8960bf317f86b357a53c4ae5f2f7d9cca174a
Kaede Kawata <85388805+schuyler1007@users.noreply.github.com>
2021-12-05 14:40:09 -0500

    Update LFSR.v

--- LFSR.v
+++ LFSR.v
@@ -2,17 +2,16 @@ module LFSR #(parameter seed = 0) (clk_i, en_i, nextbit);
     input clk_i, en_i;
     output reg nextbit;
 
-    reg [7:0] next_LFSR = seed;
+    reg [2:0] next_LFSR = seed;
 
     always@(posedge clk_i) begin
         if (en_i == 1'b1) begin
-           next_LFSR <= {next_LFSR[6:0], nextbit}; 
+           next_LFSR <= {next_LFSR[1:0], nextbit}; 
         end
     end // always
 
     always@(*) begin
-        nextbit = next_LFSR[7] ^~ next_LFSR[5] ^~ next_LFSR[4] ^~ next_LFSR[3];
+        nextbit = next_LFSR[2] ^~ next_LFSR[1];
     end // always
-
-
+    
 endmodule
