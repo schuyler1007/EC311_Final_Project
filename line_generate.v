4af2145f632ed27987b1134f63ab4dfe9de68687
bennit6 <74574676+bennit6@users.noreply.github.com>
2021-12-05 13:27:47 -0500

    Update line_generate.v
    added seed parameter

--- line_generate.v
+++ line_generate.v
@@ -1,29 +1,27 @@
-module line_generate(reset_i, clk_i, en_i, line_o);
+module line_generate #(parameter seed = 0) (reset_i, clk_i, en_i, line_o);
     input reset_i, clk_i, en_i;
     output [639:0] line_o;
-    wire [7:0] rand_num;
+    wire  rand_num;
     reg [639:0] cs = {640{1'b1}}, ns = {640{1'b1}};
     reg [6:0] count = 0;
     reg new_bit = 1;
     
-    LFSR rn(.clk_i(clk_i), .en_i(en_i), .out_o(rand_num));
+    LFSR #(.seed(seed)) rn(.clk_i(clk_i), .en_i(en_i), .nextbit(rand_num));
 
     always@(posedge clk_i or negedge reset_i) begin
         if (reset_i==0) begin
-            cs = 0;
-            cs = cs - 1;
-            ns = 0;
-            ns = ns - 1;
-            count = 0;
-            new_bit = 1;
+            cs <= {640{1'b1}};
+            ns <= {640{1'b1}};
+            count <= 0;
+            new_bit <= 1;
         end
         cs <= ns;
-        count = count + 1;
+        count <= count + 1;
     end // always
 
     always@(posedge clk_i) begin
         if (count == 80) begin
-            new_bit = rand_num[0];
+            new_bit = rand_num;
             count = 0;
         end 
         ns = {cs[638:0], new_bit};
