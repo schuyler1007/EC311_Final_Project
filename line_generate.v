c70974dad074087a45cbe03663e2efdcb0e2e367
Gough <cwgough@bu.edu>
2021-12-06 14:25:23 -0500

    updated LFSR.v and line_generate.v to match Kaede's working implementation

--- line_generate.v
+++ line_generate.v
@@ -1,32 +1,23 @@
-module line_generate #(parameter seed = 0) (reset_i, clk_i, en_i, line_o);
-    input reset_i, clk_i, en_i;
-    output [639:0] line_o;
+module line_generate #(parameter seed = 0) (reset_i, clk_i, clk_lfsr, en_i, line_o, rand_num);
+    input clk_i, reset_i, en_i, clk_lfsr;
+    output reg [639:0] line_o;
+    output wire rand_num;
     
-    wire  rand_num;
-    reg [639:0] cs = {640{1'b1}}, ns = {640{1'b1}};
-    reg [6:0] count = 0;
-    reg new_bit = 1;
+    LFSR #(.seed(seed)) rn(.clk_i(clk_lfsr), .en_i(en_i), .nextbit(rand_num));
     
-    LFSR #(.seed(seed)) rn(.clk_i(clk_i), .en_i(en_i), .nextbit(rand_num));
-
+    initial begin
+        line_o = {640{1'b1}};
+    end
+    
     always@(posedge clk_i or negedge reset_i) begin
-        if (reset_i==0) begin
-            cs <= {640{1'b1}};
-            ns <= {640{1'b1}};
-            count <= 0;
-            new_bit <= 1;
+        if (reset_i == 0) begin
+            line_o <= {640{1'b1}};
+        end else begin
+            if (line_o == {640{1'b0}})
+                line_o <= {640{1'b1}};
+            else begin
+                line_o <= {rand_num, line_o[639:1]};
+            end
         end
-        cs <= ns;
-        count <= count + 1;
-    end // always
-
-    always@(posedge clk_i) begin
-        if (count == 80) begin
-            new_bit = rand_num;
-            count = 0;
-        end 
-        ns = {cs[638:0], new_bit};
-    end // always
-    
-    assign line_o = cs;
+    end
 endmodule
