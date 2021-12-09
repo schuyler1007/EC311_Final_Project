75ce205a1cdf3cc7b10438e8c823fc471ec8b9bc
Kaede Kawata <85388805+schuyler1007@users.noreply.github.com>
2021-12-09 14:18:52 -0500

    Update LFSR.v

--- new/LFSR.v
+++ new/LFSR.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 12/05/2021 04:57:39 PM
+// Create Date: 11/28/2021 07:05:34 PM
 // Design Name: 
-// Module Name: lfsr
+// Module Name: LFSR_10bit
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -19,25 +19,32 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+
 module lfsr 
-    # (parameter Seed = 0) (
+    # (parameter Seed = 0, parameter prob = 127) (
     input clk_i,
     input rst_i,
     input en_i,
     output reg rand_o
     );
     
-    reg [2:0] next_lfsr;
+    reg next_bit;
+    reg [7:0] next_lfsr;
     
     always @(posedge clk_i or negedge rst_i) begin
         if (rst_i == 1'b0) begin
             next_lfsr <= Seed;
         end else if (en_i == 1'b1) begin
-            next_lfsr <= {next_lfsr[1:0], rand_o};
+            next_lfsr <= {next_lfsr[6:0], next_bit};
         end
     end
     
     always@(*) begin
-        rand_o = next_lfsr[2] ^~ next_lfsr[1];
+        next_bit = next_lfsr[7] ^~ next_lfsr[5] ^~ next_lfsr[4] ^~ next_lfsr[3];
+        if (next_lfsr >= prob) begin
+            rand_o = 1;
+        end else begin
+            rand_o = 0;
+        end
     end
 endmodule
